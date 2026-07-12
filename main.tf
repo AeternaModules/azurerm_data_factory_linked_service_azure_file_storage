ee4bd884ceacb0b09fed30b60c1ca869d1cb7628
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:58:09 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,7 +1,17 @@
+data "azurerm_key_vault_secret" "connection_string" {
+  for_each     = { for k, v in var.data_factory_linked_service_azure_file_storages : k => v if v.connection_string_key_vault_id != null && v.connection_string_key_vault_secret_name != null }
+  name         = each.value.connection_string_key_vault_secret_name
+  key_vault_id = each.value.connection_string_key_vault_id
+}
+data "azurerm_key_vault_secret" "password" {
+  for_each     = { for k, v in var.data_factory_linked_service_azure_file_storages : k => v if v.password_key_vault_id != null && v.password_key_vault_secret_name != null }
+  name         = each.value.password_key_vault_secret_name
+  key_vault_id = each.value.password_key_vault_id
+}
 resource "azurerm_data_factory_linked_service_azure_file_storage" "data_factory_linked_service_azure_file_storages" {
   for_each = var.data_factory_linked_service_azure_file_storages
 
-  connection_string        = each.value.connection_string
+  connection_string        = each.value.connection_string != null ? each.value.connection_string : try(data.azurerm_key_vault_secret.connection_string[each.key].value, null)
   data_factory_id          = each.value.data_factory_id
   name                     = each.value.name
   additional_properties    = each.value.additional_properties
@@ -11,7 +21,7 @@ resource "azurerm_data_factory_linked_service_azure_file_storage" "data_factory_
   host                     = each.value.host
   integration_runtime_name = each.value.integration_runtime_name
   parameters               = each.value.parameters
-  password                 = each.value.password
+  password                 = each.value.password != null ? each.value.password : try(data.azurerm_key_vault_secret.password[each.key].value, null)
   user_id                  = each.value.user_id
 
   dynamic "key_vault_password" {
--- variables.tf
+++ variables.tf
@@ -3,6 +3,8 @@ variable "data_factory_linked_service_azure_file_storages" {
 Map of data_factory_linked_service_azure_file_storages, attributes below
 Required:
     - connection_string
+    - connection_string_key_vault_id (alternative to connection_string - read from Key Vault instead)
+    - connection_string_key_vault_secret_name (alternative to connection_string - read from Key Vault instead)
     - data_factory_id
     - name
 Optional:
@@ -14,6 +16,8 @@ Optional:
     - integration_runtime_name
     - parameters
     - password
+    - password_key_vault_id (alternative to password - read from Key Vault instead)
+    - password_key_vault_secret_name (alternative to password - read from Key Vault instead)
     - user_id
     - key_vault_password (block):
         - linked_service_name (required)
@@ -21,18 +25,22 @@ Optional:
 EOT
 
   type = map(object({
-    connection_string        = string
-    data_factory_id          = string
-    name                     = string
-    additional_properties    = optional(map(string))
-    annotations              = optional(list(string))
-    description              = optional(string)
-    file_share               = optional(string)
-    host                     = optional(string)
-    integration_runtime_name = optional(string)
-    parameters               = optional(map(string))
-    password                 = optional(string)
-    user_id                  = optional(string)
+    connection_string                       = string
+    connection_string_key_vault_id          = optional(string)
+    connection_string_key_vault_secret_name = optional(string)
+    data_factory_id                         = string
+    name                                    = string
+    additional_properties                   = optional(map(string))
+    annotations                             = optional(list(string))
+    description                             = optional(string)
+    file_share                              = optional(string)
+    host                                    = optional(string)
+    integration_runtime_name                = optional(string)
+    parameters                              = optional(map(string))
+    password                                = optional(string)
+    password_key_vault_id                   = optional(string)
+    password_key_vault_secret_name          = optional(string)
+    user_id                                 = optional(string)
     key_vault_password = optional(object({
       linked_service_name = string
       secret_name         = string
